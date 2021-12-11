be6430baefbd3503c95c4518c880c7363a6fcc4b
Aviran Bachar <aviranbac1@gmail.com>
2021-12-11 22:50:34 +0200

    Adding authentication security to Data API

---
+++ data-api/src/main/java/mahat/aviran/data_api/config/OAuth2WebSecurity.java
@@ -0,0 +1,22 @@
+package mahat.aviran.data_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class OAuth2WebSecurity {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf().disable()
+                .authorizeExchange()
+                .pathMatchers("/actuator/**").permitAll()
+                .anyExchange()
+                .authenticated()
+                .and().oauth2ResourceServer().jwt();
+        return http.build();
+    }
+}
